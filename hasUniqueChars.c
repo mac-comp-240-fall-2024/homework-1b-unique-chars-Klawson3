95c91ed28510c19d6780fc73662f79fb10ac1654
klawson3 <klawson3@mscs2024.olri.macalester.edu>
2024-09-24 04:04:42 +0000

    done

--- hasUniqueChars.c
+++ hasUniqueChars.c
@@ -41,6 +41,18 @@ void seeBits(unsigned long value, char *debug_text) {
 void checkInvalid(char * inputStr) {
   char nextChar;
   int i;
+
+  // Check for NULL pointer or empty string
+    if (inputStr == NULL) {
+        fprintf(stderr, "input string is NULL\n");
+        exit(EXIT_FAILURE);
+    }
+
+        printf("Checking input string: '%s'\n", inputStr); // Debugging line
+     
+    if (inputStr[0] == '\0') {
+        return; // An empty string is considered valid, no need to check further
+    }
   
   for(i = 0; i < strlen(inputStr); i++) {
     nextChar = inputStr[i];
@@ -48,63 +60,47 @@ void checkInvalid(char * inputStr) {
     // if nextChar is invalid (31 or less or 127 as an ascii char code), then bail out
     if ((nextChar <= 31 ) || (nextChar == 127)) {
       fprintf(stderr, "invalid character in string\n");
-      exit(EXIT_FAILURE);
+      return;
     }
   }
 }
 
 
 /*
- *  This function takes an input string and checks if every character (except spaces)
+ * This function takes an input string and checks if every character (except spaces)
  * appears only once. Non-printing characters will trigger an error and cause the
  * program to exit. It uses two bit vectors to track whether characters have been seen:
  *   - checkBitsA_z for characters 'A' to 'z'
  *   - checkBitsexcl_amp for characters '!' to '@'
  * The function returns true if the string has unique characters, and false if duplicates are found.
  */
-bool hasUniqueChars(char * inputStr) {
-  // bail out quickly if any invalid characters
-  checkInvalid(inputStr);
-  
-  int i;   // loop counter
-  
-  // unsigned long can handle 64 different chars in a string
-  // if a bit at a position is 1, then we have seen that character
-  unsigned long checkBitsA_z = 0;   // for checking A through z and {|}~
-  unsigned long checkBitsexcl_amp =0;  // for checking ! though @ 
-
-  char nextChar;         // next character in string to check
-
-  // -------------------------------------------------------------
-  // This section contains code to display the initial values of checkBitsA_z
-  // and checkBitsexcl_amp, for debugging purposes. 
-  // It also illustrates how to use the seeBits function for debugging.
-  // Printed values should initially be all zeros
-  // TODO: remove or comment out this code when satisfied of function correctness
-  
-  // char debug_str_A_z[128];
-  // strcpy(debug_str_A_z, "checkBitsA_z before: \n");
-  // seeBits(checkBitsA_z, debug_str_A_z);
-  
-  // char debug_str_excl_amp[128];
-  // strcpy(debug_str_excl_amp, "checkBitsexcl_amp before: \n");
-  // seeBits(checkBitsexcl_amp, debug_str_excl_amp);
-  // -------------------------------------------------------------
-
-  // TODO: Declare additional variables you need here
-
-    unsigned long mask;
+bool hasUniqueChars(char *inputStr) {
 
   
-  for(i = 0; i < strlen(inputStr); i++) {
-    nextChar = inputStr[i];
-      
-      // Ignore spaces
-    if (nextChar == ' ') {
-      continue;
+    // Check for invalid characters
+    checkInvalid(inputStr);
+
+     // Return true immediately if the string is empty
+    if (inputStr[0] == '\0') {
+        return true; // Empty string has no duplicate characters
     }
 
-    if (nextChar >= 'A' && nextChar <= 'Z') {
+    int i;
+    unsigned long checkBitsA_z = 0;     // for checking A through z and {|}~
+    unsigned long checkBitsexcl_amp = 0; // for checking ! through @
+    char nextChar;
+    unsigned long mask;
+
+    for (i = 0; i < strlen(inputStr); i++) {
+        nextChar = inputStr[i];
+
+        // Ignore spaces
+        if (nextChar == ' ') {
+            continue;
+        }
+
+        // Handle uppercase letters 'A' to 'Z'
+        if (nextChar >= 'A' && nextChar <= 'Z') {
             int bitPosition = nextChar - 'A'; // Calculate bit position for uppercase letters
             mask = 1UL << bitPosition;
 
@@ -114,8 +110,10 @@ bool hasUniqueChars(char * inputStr) {
             }
             // Otherwise, set the bit in the bit vector
             checkBitsA_z |= mask;
-
-        } else if (nextChar >= 'a' && nextChar <= 'z') {
+        } 
+        
+        // Handle lowercase letters 'a' to 'z'
+        else if (nextChar >= 'a' && nextChar <= 'z') {
             int bitPosition = nextChar - 'a' + 26; // Offset for lowercase letters
             mask = 1UL << bitPosition;
 
@@ -140,25 +138,7 @@ bool hasUniqueChars(char * inputStr) {
             checkBitsexcl_amp |= mask;
         }
     }
-    
-    return true;
-    // -------------------------------------------------------------
-    // Below this are examples of debugging print statements you could use
-    // Move/use as makes sense for you!
-    // Modify to work on checkBitsexcl_amp
-    // TODO: Comment out or remove when your function works correctly
-    // printf("nextchar int value: %d\n", nextChar);
-    // char char_str[2] = "\0";
-    // char_str[0] = nextChar;
-    // strcpy(debug_str_A_z, "nextchar: ");
-    // strcat(debug_str_A_z, char_str);
-    // strcat(debug_str_A_z,", checkBitsA_z: \n");
-    // seeBits(checkBitsA_z, debug_str_A_z);
-    // ------------------------------------------------------------- 
-  }
-
-  // if through all the characters, then no duplicates found
-  
+}
 
 
   
